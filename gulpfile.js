4e31c7003048b4150ef003286c1b2ea51c04062c
九条涼果 <s1131234@gmail.com>
2018-05-21 14:52:27 +0800

    添加CDN，使用FontAwesome5

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
-const cleanCss = require('gulp-clean-css')
-const gulp = require('gulp')
-const rename = require('gulp-rename')
-const sass = require('gulp-sass')
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const cleanCss = require('gulp-clean-css');
 
-gulp.task('default', ['stylesheet'])
 
-gulp.task('stylesheet', function () {
+gulp.task('watch', ['default'], function() {
+    gulp.watch('./assets/stylesheets/**/*.scss', ['stylesheet']);
+});
+
+gulp.task('default', ['stylesheet']);
+
+gulp.task('stylesheet', function() {
     return gulp.src('./assets/stylesheets/index.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(rename('index.bundle.css'))
         .pipe(gulp.dest('./assets/stylesheets/'))
         .pipe(cleanCss())
         .pipe(rename('index.bundle.min.css'))
-        .pipe(gulp.dest('./assets/stylesheets/'))
-})
-
-gulp.task('watch', ['default'], function () {
-    gulp.watch('./assets/stylesheets/**/*.scss', ['stylesheet'])
-})
+        .pipe(gulp.dest('./assets/stylesheets/'));
+});
